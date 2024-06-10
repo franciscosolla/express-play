a9808acb0dff0a926afbe49855c03949a99f8597
Francisco Solla <francisco.solla@gmail.com>
2024-06-10 02:26:37 +0200

    deploy

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (_, res) => {
-  res.send("<h1>Hello World!</h1><h2>This is my Express.js playground</h2>");
+  res.send("<h1>Hello World!</h1><h2>This is my Express.js playground.</h2>");
 });
 
 module.exports = app;
