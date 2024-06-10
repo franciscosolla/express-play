4976cbce3aab9d1e436914686b77152230d20699
Francisco Solla <francisco.solla@gmail.com>
2024-06-10 02:44:26 +0200

    separate vue into its own folder

--- api/index.ts
+++ api/index.ts
@@ -3,8 +3,14 @@ import path from "path";
 
 const app = express();
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "src/index.html"));
+app.get("/vue*", (_, res) => {
+  res.sendFile(path.join(__dirname, "..", "src/vue/index.html"));
+});
+
+app.get("/*", (_, res) => {
+  res.send(
+    "<h1>This is my Express.js playground<h1><a href='/vue'>Go to Vue.js</a>",
+  );
 });
 
 module.exports = app;
